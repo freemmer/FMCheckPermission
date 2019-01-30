b80d3fedb6effb72d7f50abdafc6dd83904cea83
freemmer-iMac <freemmer@gmail.com>
2019-01-30 16:57:34 +0900

    -Modified to use 'function parameter'

--- app/src/main/java/com/tistory/freemmer/lib/fmcheckpermission/demo/MainActivity.kt
+++ app/src/main/java/com/tistory/freemmer/lib/fmcheckpermission/demo/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.tistory.freemmer.lib.fmcheckpermission.demo
 
 import android.Manifest
-import android.app.Activity
-import android.app.AlertDialog
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.view.View
-import com.tistory.freemmer.lib.fmcheckpermission.FMCheckPermission
-import com.tistory.freemmer.lib.fmcheckpermission.FMCheckPermissionSystemAlert
+import com.tistory.freemmer.lib.fmcheckpermission.FMCheckPermissionActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 
-class MainActivity : Activity() {
+class MainActivity : FMCheckPermissionActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,76 +21,32 @@ class MainActivity : Activity() {
         }
 
         btnCheckPermission.setOnClickListener {
-            checkPermission(it)
+            checkPermission(
+                arrayOf(
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW)
+                , {
+                    // All Permissions requested are allowed
+                    Snackbar.make(btnCheckPermission
+                        , "OK!!", Snackbar.LENGTH_SHORT).show()
+                }, {checkedDoNotAskPermissions, permissions ->
+                    // Requested Permission denied
+                    if (checkedDoNotAskPermissions.isNotEmpty()) {
+                        Snackbar.make(btnCheckPermission
+                            , "Requested Permissions denied with 'Don't ask again' : $checkedDoNotAskPermissions"
+                            , Snackbar.LENGTH_LONG)
+                            .setAction("move setting") { movePermissionSetting() }.show()
+                    } else {
+                        Snackbar.make(btnCheckPermission
+                            , "Requested Permission denied : $permissions", Snackbar.LENGTH_SHORT).show()
+                    }
+                })
         }
 
-        btnCheckPermissionSystemAlertWindow.setOnClickListener {
-            checkPermissionSystemAlert(it)
-        }
-
     }
-
-    /**
-     * Check permissions to except 'SYSTEM_ALERT_WINDOW'
-     */
-    private fun checkPermission(view: View) {
-        FMCheckPermission.Builder.build(arrayOf(
-            Manifest.permission.READ_PHONE_STATE,
-            Manifest.permission.READ_CONTACTS,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ))
-        {
-            pGrantedRequestPermission = {
-                Snackbar.make(view, "Granted Permission (퍼미션 획득 됨)", Snackbar.LENGTH_SHORT)
-                    .show()
-            }
-            pDeniedRequestPermission = { checkPermissionActivity, permissions ->
-                val alert = AlertDialog.Builder(checkPermissionActivity).create()
-                alert.setTitle("Info")
-                alert.setMessage("Denied Permission (거부됨) : $permissions")
-                alert.setButton(AlertDialog.BUTTON_POSITIVE, "Try again") { dialog, id ->
-                    checkPermissionActivity.execute()
-                    dialog.dismiss()
-                }
-                alert.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel") { dialog, id ->
-                    checkPermissionActivity.finish()
-                    dialog.dismiss()
-                }
-                alert.show()
-            }
-            pRequestPermissionRationale = { checkPermissionActivity, permissions ->
-                val alert = AlertDialog.Builder(checkPermissionActivity).create()
-                alert.setTitle("Info")
-                alert.setMessage("Request permission that checked 'Don't ask again'(다시보지 않음을 선택한 퍼미션이 요청됨) : $permissions")
-                alert.setButton(AlertDialog.BUTTON_POSITIVE, "Move Setting") { dialog, id ->
-                    checkPermissionActivity.defaultRequestPermissionRationale()
-                    dialog.dismiss()
-                }
-                alert.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel") { dialog, id ->
-                    checkPermissionActivity.finish()
-                    dialog.dismiss()
-                }
-                alert.show()
-            }
-        }.execute(this)
-    }
-
-    /**
-     * Check permission accept to 'SYSTEM_ALERT_WINDOW'
-     */
-    private fun checkPermissionSystemAlert(view: View) {
-        FMCheckPermissionSystemAlert.Builder.build {
-            pDeniedPermission = {
-                Snackbar.make(view, "System Alert Window : Denied Permission (거부됨)", Snackbar.LENGTH_SHORT)
-                    .setAction("Action", null).show()
-            }
-            pGrantedPermission = {
-                Snackbar.make(view, "System Alert Window : Granted Permission (권한획득)", Snackbar.LENGTH_SHORT)
-                    .setAction("Action", null).show()
-            }
-        }.execute(this)
-    }
-
 
 }
+
--- fmcheckpermission/src/main/java/com/tistory/freemmer/lib/fmcheckpermission/FMCheckPermission.kt
+++ fmcheckpermission/src/main/java/com/tistory/freemmer/lib/fmcheckpermission/FMCheckPermission.kt
@@ -1,182 +1,213 @@
 package com.tistory.freemmer.lib.fmcheckpermission
 
+import android.Manifest
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import android.os.Bundle
 import android.provider.Settings
 import android.support.v4.app.ActivityCompat
+import android.support.v4.app.FragmentActivity
 import android.support.v4.content.ContextCompat
-import android.util.Log
-import java.util.ArrayList
+import android.support.v7.app.AppCompatActivity
+import java.lang.ref.WeakReference
+import kotlin.collections.ArrayList
 
 /**
  * Created by freemmer on 11/01/2019.
  * History
  *    - 11/01/2019 Create file
  */
-
-/**
- * Android 8.0 이상부터 사용되는 동적 퍼미션 체크를 지원한다
- * @param permissions   요청할 퍼미션
- * @param pRequestPermissionRationale   완전 거부한 퍼미션이 요청되었을때 호출됨
- *                                      (설정으로 이동 해야 함), 퍼미션 체크가 끝날경우 FMCheckPermissionActivity를 finish해야함
- * @param pDeniedRequestPermission      접근권한 요청 거부되었을때 호출됨
- *                                      퍼미션 체크가 끝날경우 FMCheckPermissionActivity를 finish해야함
- * @param pGrantedRequestPermission     퍼미션 획득 되었을때 호출됨
- */
-class FMCheckPermission (
-    var permissions: Array<String>,
-    var pRequestPermissionRationale: ((FMCheckPermissionActivity: FMCheckPermissionActivity, permissions: List<String>) -> Unit)?,
-    var pDeniedRequestPermission: ((FMCheckPermissionActivity: FMCheckPermissionActivity, permissions: List<String>) -> Unit)?,
-    var pGrantedRequestPermission: (() -> Unit)?
+class FMCheckPermission private constructor(
+    private val activity: Activity
 ) {
-    class Builder(private var permissions: Array<String>, init: Builder.() -> Unit) {
-        /* 완전 거부한 퍼미션이 요청됨 (설정으로 이동 해야 함), 퍼미션 체크가 끝날경우 checkPermissionActivity를 finish해야함 */
-        var pRequestPermissionRationale: ((FMCheckPermissionActivity: FMCheckPermissionActivity, permissions: List<String>) -> Unit)? = null
-        /* 접근권한 요청 거부됨, 퍼미션 체크가 끝날경우 checkPermissionActivity를 finish해야함 */
-        var pDeniedRequestPermission: ((FMCheckPermissionActivity: FMCheckPermissionActivity, permissions: List<String>) -> Unit)? = null
-        /* 퍼미션 획득 됨 */
-        var pGrantedRequestPermission: (() -> Unit)? = null
-
-        init {
-            init()
-        }
-
-        fun build() : FMCheckPermission {
-            return FMCheckPermission(permissions
-                , pRequestPermissionRationale, pDeniedRequestPermission, pGrantedRequestPermission)
-        }
-
-        companion object {
-            fun build(permissions: Array<String>, init: Builder.() -> Unit) = Builder(permissions, init).build()
-        }
-    }
-
-
-    fun execute(activity: Activity) {
-        if (Build.VERSION.SDK_INT >= 23) {
-            FMCheckPermissionActivity.serializable = this
-            val intent = Intent(activity, FMCheckPermissionActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            activity.startActivity(intent)
-        } else {
-            pGrantedRequestPermission?.invoke()
-        }
-    }
 
     companion object {
-        fun build(permissions: Array<String>, init: Builder.() -> Unit) = Builder(permissions, init)
-    }
+        private var weakReference: WeakReference<FMCheckPermission>? = null
 
-}
-
-
-
-class FMCheckPermissionActivity
-    : Activity()
-{
-    companion object {
-        const val REQUEST_PERMISSIONS_CHECK = 100
-        const val REQUEST_SETTING           = 101
-        lateinit var serializable: FMCheckPermission
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_check_permission)
-        execute()
-    }
-
-    override fun finish() {
-        super.finish()
-        overridePendingTransition(R.anim.loadfadein, R.anim.loadfadeout)
-    }
-
-
-    fun execute() {
-        if (serializable.permissions.isNotEmpty()) {
-            checkPermission(serializable.permissions)
-        } else {
-            finish()
+        fun instance(activity: Activity): FMCheckPermission {
+            if (weakReference?.get() == null) {
+                weakReference = WeakReference(FMCheckPermission(activity))
+            }
+            return weakReference?.get()!!
         }
     }
 
+    private val REQUEST_PERMISSIONS_CHECK   = 3000
+    private val OVERLAY_PERMISSION_REQ_CODE = 3001
+    private val REQUEST_SETTING             = 3002
 
-    private fun checkPermission(permissions: Array<String>) {
-        val needPermissionsList = ArrayList<String>()
+    private lateinit var pAllowedFunc: (() -> Unit)
+    private lateinit var pDeniedFunc: ((checkedDoNotAskPermissions: Array<String>, permissions: Array<String>) -> Unit)
+    private val list = ArrayList<String>()
+    private var managerOverlayIntent: Intent? = null
 
-        for (permission in permissions) { // 권한 체크
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                // 권한이 없을 경우
+    fun check(permissions: Array<String>
+              , pAllowedFunc:() -> Unit
+              , pDeniedFunc:(checkedDoNotAskPermissions: Array<String>, permissions: Array<String>) -> Unit) {
+        this.pAllowedFunc = pAllowedFunc
+        this.pDeniedFunc = pDeniedFunc
+        list.clear()
+        managerOverlayIntent = null
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (item in permissions) {
+                if (item == Manifest.permission.SYSTEM_ALERT_WINDOW) {
+                    if (Settings.canDrawOverlays(activity)) {
+                        // SYSTEM_ALERT_WINDOW 허용됨
+                    } else  {
+                        managerOverlayIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION
+                            , Uri.parse("package:${activity.applicationContext.packageName}"))
+                        activity.startActivityForResult(managerOverlayIntent, OVERLAY_PERMISSION_REQ_CODE)
+                        return
+                    }
+                } else {
+                    list.add(item)
+                }
+            }
+            checkNormalPermission(list.toTypedArray())
+        } else {
+            pAllowedFunc.invoke()
+        }
+    }
+
+    private fun checkNormalPermission(permissions: Array<String>) {
+        val needPermissionsList = ArrayList<String>() // 권한이 없는 Permission list
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 needPermissionsList.add(permission)
             }
         }
-        if (needPermissionsList.isNotEmpty()) {
-            Log.d(FMCheckPermission::class.java.simpleName, "Request permission: $needPermissionsList")
-            ActivityCompat.requestPermissions(this, needPermissionsList.toTypedArray(), REQUEST_PERMISSIONS_CHECK)
+        if (needPermissionsList.isNotEmpty()) { // 접근권한 요청: $needPermissionsList
+            ActivityCompat.requestPermissions(activity, needPermissionsList.toTypedArray(), REQUEST_PERMISSIONS_CHECK)
         } else {
-            serializable.pGrantedRequestPermission?.invoke()
-            finish()
+           pAllowedFunc.invoke()
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_PERMISSIONS_CHECK) {
-            val deniedPermissionsList = ArrayList<String>()
-            val rationaleDeniedPermissions = ArrayList<String>()
-            for (i in permissions.indices) {
-                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {
-                        deniedPermissionsList.add(permissions[i])
-                    } else {
-                        rationaleDeniedPermissions.add(permissions[i])
-                    }
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode != REQUEST_PERMISSIONS_CHECK) return
+
+        val deniedPermissionsList = ArrayList<String>()      // 완전 거부한 퍼미션 (Checked 'Don't ask')
+        val rationaleDeniedPermissions = ArrayList<String>() // 거부한 퍼미션 (일반)
+        for (i in permissions.indices) {
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permissions[i])) {
+                    deniedPermissionsList.add(permissions[i])
+                } else {
+                    rationaleDeniedPermissions.add(permissions[i])
                 }
             }
-            if (rationaleDeniedPermissions.isNotEmpty()) {
-                Log.d(FMCheckPermission::class.java.simpleName, "Permission Rejected $rationaleDeniedPermissions (must be move to the setting)")
-                if (serializable.pRequestPermissionRationale != null) {
-                    serializable.pRequestPermissionRationale!!.invoke(this, rationaleDeniedPermissions)
-                } else {
-                    defaultRequestPermissionRationale()
-                }
-            } else if (deniedPermissionsList.isNotEmpty()) {
-                Log.d(FMCheckPermission::class.java.simpleName, "Permission Rejected")
-                if (serializable.pDeniedRequestPermission != null) {
-                    serializable.pDeniedRequestPermission!!.invoke(this, deniedPermissionsList)
-                } else {
-                    finish()
-                }
-            } else {
-                Log.d(FMCheckPermission::class.java.simpleName, "Permission Accepted")
-                serializable.pGrantedRequestPermission?.invoke()
-                finish()
-            }
+        }
+        if (rationaleDeniedPermissions.isEmpty() && deniedPermissionsList.isEmpty()) {
+            pAllowedFunc.invoke()
+        } else {
+            pDeniedFunc.invoke(rationaleDeniedPermissions.toTypedArray(), deniedPermissionsList.toTypedArray())
         }
     }
 
+    fun onActivityResult(requestCode: Int) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(activity)) {
+                    checkNormalPermission(list.toTypedArray())
+                } else {
+                    list.add(Manifest.permission.SYSTEM_ALERT_WINDOW)
+                    pDeniedFunc.invoke(arrayOf(), list.toTypedArray())
+                }
+            }
+        } else if (requestCode == REQUEST_SETTING) {
+            checkNormalPermission(list.toTypedArray())
+        }
+    }
 
-    fun defaultRequestPermissionRationale() {
-        val packageName = applicationContext.packageName
+    fun moveSetting() {
         try {
             val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                .setData(Uri.parse("package:$packageName"))
-            startActivityForResult(intent, REQUEST_SETTING)
+                .setData(Uri.parse("package:${activity.applicationContext.packageName}"))
+            activity.startActivityForResult(intent, REQUEST_SETTING)
         } catch (e: ActivityNotFoundException) {
             val intent = Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS)
-            startActivityForResult(intent, REQUEST_SETTING)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_SETTING) {
-            execute()
+            activity.startActivityForResult(intent, REQUEST_SETTING)
         }
     }
 
 }
+
+
+
+
+abstract class FMCheckPermissionActivity: Activity() {
+    private lateinit var checker: FMCheckPermission
+
+    fun checkPermission(permissions: Array<String>
+                        , pAllowedFunc:() -> Unit
+                        , pDeniedFunc:(checkedDoNotAskPermissions: Array<String>, permissions: Array<String>) -> Unit)
+    {
+        checker = FMCheckPermission.instance(this)
+        checker.check(permissions, pAllowedFunc, pDeniedFunc)
+    }
+
+    fun movePermissionSetting() {
+        checker.moveSetting()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        checker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        checker.onActivityResult(requestCode)
+    }
+}
+
+abstract class FMCheckPermissionAppCompatActivity: AppCompatActivity() {
+    private lateinit var checker: FMCheckPermission
+
+    fun checkPermission(permissions: Array<String>
+                        , pAllowedFunc:() -> Unit
+                        , pDeniedFunc:(checkedDoNotAskPermissions: Array<String>, permissions: Array<String>) -> Unit)
+    {
+        checker = FMCheckPermission.instance(this)
+        checker.check(permissions, pAllowedFunc, pDeniedFunc)
+    }
+
+    fun movePermissionSetting() {
+        checker.moveSetting()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        checker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        checker.onActivityResult(requestCode)
+    }
+}
+
+abstract class FMCheckPermissionAppFragmentActivity: FragmentActivity() {
+    private lateinit var checker: FMCheckPermission
+
+    fun checkPermission(permissions: Array<String>
+                        , pAllowedFunc:() -> Unit
+                        , pDeniedFunc:(checkedDoNotAskPermissions: Array<String>, permissions: Array<String>) -> Unit)
+    {
+        checker = FMCheckPermission.instance(this)
+        checker.check(permissions, pAllowedFunc, pDeniedFunc)
+    }
+
+    fun movePermissionSetting() {
+        checker.moveSetting()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        checker.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        checker.onActivityResult(requestCode)
+    }
+}
+
